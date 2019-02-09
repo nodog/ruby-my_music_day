86adfcd7128a27f7803b5f8a311be54dc77e6890
Anderson Mills <nodog@konfuzo.net>
2019-02-08 23:17:13 -0600

    Add random_repertoire and update for Oleo.

---
+++ random_repertoire.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+repertoire = ['Take the A Train',
+              'Blue Monk',
+              'Summertime',
+              'So What',
+              'Blue Skies',
+              'All of Me',
+              'Freddie Freeloader',
+              'Chameleon']
+
+repertoire = repertoire.shuffle
+
+puts
+puts '---------------------------'
+for i in 0..(repertoire.size - 1)
+  puts repertoire[i]
+end
+puts '---------------------------'
+puts
