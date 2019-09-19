96a45e20ed855bf41e42e9cc255b9b7261ed7c43
Anderson Mills <nodog@konfuzo.net>
2019-09-19 06:12:58 -0500

    Update for current practice and lessons.

--- random_instrument_order.rb
+++ random_instrument_order.rb
@@ -22,18 +22,24 @@ end
 
 prng = Random.new(seed)
 
-instruments = ['voice']
+instruments = ['45 - voice']
 
-if seed.even?()
-  instruments.push('bass')
+if prng.rand() < 0.5
+  instruments.push('30 - bass')
 else
-  instruments.push('guitar')
+  instruments.push('30 - guitar')
+end
+
+if prng.rand() < 0.5
+  instruments.push('30 - piano - xscription')
+else
+  instruments.push('30 - piano - repertoire')
 end
 
 instruments = instruments.shuffle(random: prng)
 
 drums_placement = prng.rand(instruments.size - 1) + 1
-instruments.insert(drums_placement, 'drums')
+instruments.insert(drums_placement, '45 - drums')
 
 # piano skills anywhere
 piano_skills_placement = prng.rand(instruments.size)
@@ -48,8 +54,9 @@ if piano_skills_placement > 2
   piano_songs_placement = prng.rand(3)
 end
 
-instruments.insert(piano_skills_placement, 'piano - skills')
-instruments.insert(piano_songs_placement, 'piano - new songs')
+instruments.insert(piano_skills_placement, '30 - piano - skills')
+instruments.insert(piano_songs_placement, '30 - piano - new songs')
+instruments.insert(0, '15 - piano - warmups')
 
 puts
 puts '---------------------------'
